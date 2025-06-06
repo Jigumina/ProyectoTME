81db2b69268c8ce73e1b398a720be2570911af3f
aurielcp <auriel.castroperez@gmail.com>
2025-06-05 23:55:01 -0600

    Clase Veterinario

--- VetLogix.2/VetLogix/src/main/java/mx/fes/aragon/ico/VetLogix/model/Veterinario.java
+++ VetLogix.2/VetLogix/src/main/java/mx/fes/aragon/ico/VetLogix/model/Veterinario.java
@@ -13,7 +13,6 @@ public class Veterinario {
     private String especialidad;
     private String telefono;
 
-    // Getters y setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
